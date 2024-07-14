88e513088d05d36934ce8295e5c07c724592e325
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-07-14 14:30:08 -0700

    Add function to display existing blog posts via element creation (working)

--- assets/js/form.js
+++ assets/js/form.js
@@ -8,6 +8,44 @@ const existingPosts = existingPostsString.startsWith("[")
   : [];
 console.log(existingPosts);
 
+populateExistingPostsDisplay();
+
+function populateExistingPostsDisplay() {
+
+  const mainPanel = document.getElementById('main-panel');
+
+  for(const blogPostObject of existingPosts){
+    const displayCard = document.createElement('div');
+    displayCard.classList.add('blog-card');
+    
+    const userName = createBlogPostElement(blogPostObject, 'userName');
+    const blogTitle = createBlogPostElement(blogPostObject, 'blogTitle');
+    const blogContent = createBlogPostElement(blogPostObject, 'blogContent');
+
+    displayCard.appendChild(blogTitle);
+    displayCard.appendChild(userName);
+    displayCard.appendChild(blogContent);
+
+    mainPanel.appendChild(displayCard);
+  }
+}
+
+
+function createBlogPostElement(blogPostObject, blogPostProperty){
+  const blogPostElementToClassMap = {
+    blogTitle: 'blog-card-title',
+    userName: 'blog-card-username'
+  };
+
+  const element = document.createElement('div');
+  element.classList.add(blogPostElementToClassMap[blogPostProperty]);
+
+  element.innerHTML = blogPostProperty == 'userName' ? `Written by ${blogPostObject[blogPostProperty]}` : blogPostObject[blogPostProperty];
+
+  return element;
+}
+
+
 // Create button listener
 document.addEventListener("click", storeBlogEntry);
 
