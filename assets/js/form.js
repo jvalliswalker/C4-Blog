8da62a6c78812dc8a3bc8350ca84210c2eb58e4f
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-07-14 14:02:26 -0700

    Apply formatter

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,33 +1,36 @@
-
-
 // Start by querying local storage for already existing blog entries
-const existingPostsString = localStorage.getItem('existing-posts');
+const existingPostsString = localStorage.getItem("existing-posts");
 
 // Set existingPosts to queried local storage data if that data
 // can be converted to an array, otherwise set as empty array
-const existingPosts = existingPostsString.startsWith('[') ? JSON.parse(existingPostsString) : [];
+const existingPosts = existingPostsString.startsWith("[")
+  ? JSON.parse(existingPostsString)
+  : [];
 console.log(existingPosts);
 
 // Create button listener
-document.addEventListener('click', storeBlogEntry);
+document.addEventListener("click", storeBlogEntry);
 
-function storeBlogEntry(event){
+function storeBlogEntry(event) {
   // Check if clicked button is submit button
-  if(event.target.tagName == 'BUTTON' && event.target.id == 'submit-blog-post'){
+  if (
+    event.target.tagName == "BUTTON" &&
+    event.target.id == "submit-blog-post"
+  ) {
     event.preventDefault();
 
     // Construct blog post object from form fields
     const blogPost = {
-      userName: document.getElementById('input-username').value,
-      blogTitle: document.getElementById('input-blog-title').value,
-      blogContent: document.getElementById('input-blog-content').value,
-      createdDate: new Date()
-    }
+      userName: document.getElementById("input-username").value,
+      blogTitle: document.getElementById("input-blog-title").value,
+      blogContent: document.getElementById("input-blog-content").value,
+      createdDate: new Date(),
+    };
 
     // Add new post to existing posts array
     existingPosts.push(blogPost);
 
     // Stringify existing posts and set to local storage
-    localStorage.setItem('existing-posts', JSON.stringify(existingPosts));
+    localStorage.setItem("existing-posts", JSON.stringify(existingPosts));
   }
 }
