34e2c699f141359e3a9ff9d622e106ae07a43e0d
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-07-18 19:13:52 -0700

    Add field validation for blog form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,52 @@
 // Create button listener
 document.addEventListener("click", storeBlogEntry);
 
+function getFormElements(){
+
+
+  return blogPostObject;
+}
+
+function validatePost(){
+
+    let isValid = true;
+
+    // Construct blog post object from form fields
+    const blogPost = {
+      userName: document.getElementById("input-username"),
+      blogTitle: document.getElementById("input-blog-title"),
+      blogContent: document.getElementById("input-blog-content"),
+      createdDate: new Date(),
+    };
+
+    Object.keys(blogPost).forEach( key => {
+
+      if(key != 'createdDate'){
+        const element = blogPost[key];
+        console.log('el: '+element);
+        
+        if(element.value == null || element.value.length < 3){
+          element.classList.add('invalid');
+          element.placeholder = 'This field is required';
+          isValid = false;
+        }
+        else {
+          element.classList.remove('invalid');
+          console.log('elment.value: '+element.value);
+          blogPost[key] = element.value;
+        }
+      }
+    });
+
+    if(isValid){
+      return blogPost;
+    }
+    else {
+      return null;
+    }
+}
+
+
 function storeBlogEntry(event) {
   // Check if clicked button is submit button
   if (
@@ -8,22 +54,18 @@ function storeBlogEntry(event) {
     event.target.id == "submit-button"
   ) {
     event.preventDefault();
+    
+    const blogPost = validatePost();
 
-    // Construct blog post object from form fields
-    const blogPost = {
-      userName: document.getElementById("input-username").value,
-      blogTitle: document.getElementById("input-blog-title").value,
-      blogContent: document.getElementById("input-blog-content").value,
-      createdDate: new Date(),
-    };
-
-    // Add new post to existing posts array
-    existingPosts.push(blogPost);
-
-    // Stringify existing posts and set to local storage
-    localStorage.setItem("existing-posts", JSON.stringify(existingPosts));
-
-    // Redirect to blog page
-    location.href = 'blog.html';
+    if(blogPost != null) {
+      // Add new post to existing posts array
+      existingPosts.push(blogPost);
+  
+      // Stringify existing posts and set to local storage
+      localStorage.setItem("existing-posts", JSON.stringify(existingPosts));
+  
+      // Redirect to blog page
+      location.href = 'blog.html';
+    }
   }
 }
