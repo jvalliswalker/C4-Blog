2391ecfa3ee679e9e02bce3f07e47de54ba3cf35
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-07-21 11:25:31 -0700

    Remove commented out code, apply formatter

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,19 +1,16 @@
 // Variables //
-const header = document.querySelector('header');
-const footer = document.querySelector('footer');
-const profileHyperlink = 'https://jvalliswalker.github.io/C2-Profile/';
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+const profileHyperlink = "https://jvalliswalker.github.io/C2-Profile/";
 let existingPosts = [];
 
-
-// Script //
+// Execution //
 extractLocalStorageData();
-// populateHeader();
 populateFooter();
-
 
 // Functions //
 
-// Gets existing-posts local storage 
+// Gets existing-posts local storage
 function extractLocalStorageData() {
   const existingPostsString = localStorage.getItem("existing-posts");
 
@@ -22,26 +19,16 @@ function extractLocalStorageData() {
   }
 }
 
-// // Create standard headers across pages
-// function populateHeader() {
-//   const headerDiv = document.createElement('div');
-  
-//   headerDiv.innerText = 'My Fancy Blog';
-
-//   header.appendChild(headerDiv);
-// }
-
 // Create standard footers across pages
 function populateFooter() {
-  const footerDiv = document.createElement('div');
-  const profileAnchor = document.createElement('a');
+  const footerDiv = document.createElement("div");
+  const profileAnchor = document.createElement("a");
 
-  profileAnchor.setAttribute('href', profileHyperlink);
-  profileAnchor.setAttribute('target', '_blank');
-  profileAnchor.innerText = 'Jamil Vallis-Walker';
+  profileAnchor.setAttribute("href", profileHyperlink);
+  profileAnchor.setAttribute("target", "_blank");
+  profileAnchor.innerText = "Jamil Vallis-Walker";
 
-
-  footerDiv.innerText = 'Site by ';
+  footerDiv.innerText = "Site by ";
   footerDiv.appendChild(profileAnchor);
 
   footer.appendChild(footerDiv);
