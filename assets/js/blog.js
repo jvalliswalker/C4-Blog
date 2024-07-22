738c385df1808c80df65517c808c258b43353bf1
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-07-21 20:04:50 -0700

    Clean up, add descriptions, apply formatter

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,70 +1,69 @@
 // Variables
-// let existingPosts = [];
 let isDarkMode = false;
 
 // Execution
 populateExistingPostsDisplay();
 document.body.addEventListener("click", handleModeChange);
-document.getElementById('navigate-back').addEventListener("click", navigateToLandingPage);
-
+document
+  .getElementById("navigate-back")
+  .addEventListener("click", navigateToLandingPage);
 
 // Functions
-function navigateToLandingPage(){
-  location.href = 'index.html';
+function navigateToLandingPage() {
+  location.href = "index.html";
 }
 
-
-// Toggle light/dark mode 
+// Toggle light/dark mode
 function handleModeChange(event) {
   if (event.target.id == "sun-icon") {
+    // Toggle mode on click
+    isDarkMode = !isDarkMode;
+    // Query elements to modify
     const blogCards = document.querySelectorAll(".blog-card");
-    const header = document.querySelector('header');
-    const sunIcon = document.getElementById('sun-icon');
+    const header = document.querySelector("header");
+    const sunIcon = document.getElementById("sun-icon");
 
-    if(isDarkMode){
+    if (isDarkMode == true) {
+      // Apply Invert filter to each blog card
+      blogCards.forEach((card) => {
+        card.classList.remove("regular-filter");
+        card.classList.add("inverted-filter");
+      });
+
+      // Apply "dark" css class to header and body
+      header.classList.add("dark-header");
+      document.body.classList.add("dark-scrollbar");
+
+      // Change icon from sun to moon
+      sunIcon.setAttribute("src", "https://openclipart.org/image/800px/170669");
+    } else {
+      // Remove invert filter from each blog card and restore regular filter
       blogCards.forEach((card) => {
         card.classList.remove("inverted-filter");
         card.classList.add("regular-filter");
       });
-      
-      header.classList.remove('dark-header');
-      document.body.classList.remove('dark-scrollbar');
-      
-      sunIcon.setAttribute(
-        'src', 'https://openclipart.org/image/800px/27381'
-      );
-    }
-    else { 
-      blogCards.forEach((card) => {
-        card.classList.remove("regular-filter");
-        card.classList.add("inverted-filter")
-      });
-      
-      header.classList.add('dark-header');
-      document.body.classList.add('dark-scrollbar');
-      
-      sunIcon.setAttribute(
-        'src', 'https://openclipart.org/image/800px/170669'
-      );
-    }
 
-    isDarkMode = !isDarkMode;
+      // Remove "dark" css class from header and boyd
+      header.classList.remove("dark-header");
+      document.body.classList.remove("dark-scrollbar");
+
+      // Change icon from moon to sun
+      sunIcon.setAttribute("src", "https://openclipart.org/image/800px/27381");
+    }
   }
 }
 
 // Sorts passed Blog post objects by created date, putting most
 // recently created posts first
-function sortByDate(a, b){
+function sortByDate(a, b) {
   const dateA = new Date(a.createdDate);
   const dateB = new Date(b.createdDate);
 
-  if (dateA < dateB){
+  if (dateA < dateB) {
     return 1;
-  }
-  else if (dateA > dateB){
-    return -1
-  }
-  else {
+  } else if (dateA > dateB) {
+    return -1;
+  } else {
     return 0;
   }
 }
@@ -73,50 +72,61 @@ function sortByDate(a, b){
 function populateExistingPostsDisplay() {
   const mainPanel = document.getElementById("blog-posts");
 
-  if(existingPosts.length > 0){
+  // Check that existingPosts contains data (var populated in logic.js)
+  if (existingPosts.length > 0) {
+    // Sort posts by created date (descending)
     existingPosts.sort(sortByDate);
-  }
 
-  for (const blogPostObject of existingPosts) {
-    const displayCard = document.createElement("div");
-    displayCard.classList.add("blog-card");
+    // Create parent and child elements for each object in existingPosts
+    for (const blogPostObject of existingPosts) {
+      // Create parent element and apply styling
+      const displayCard = document.createElement("div");
+      displayCard.classList.add("blog-card");
 
-    const blogTitle = createBlogPostElement(blogPostObject, "blogTitle");
-    const userName = createBlogPostElement(blogPostObject, "userName");
-    const createdDate = createBlogPostElement(blogPostObject, "createdDate");
-    const blogContent = createBlogPostElement(blogPostObject, "blogContent");
+      // Create child element for each parameter
+      const blogTitle = createBlogPostElement(blogPostObject, "blogTitle");
+      const userName = createBlogPostElement(blogPostObject, "userName");
+      const createdDate = createBlogPostElement(blogPostObject, "createdDate");
+      const blogContent = createBlogPostElement(blogPostObject, "blogContent");
 
-    displayCard.appendChild(blogTitle);
-    displayCard.appendChild(userName);
-    displayCard.appendChild(createdDate);
-    displayCard.appendChild(blogContent);
+      // append child elements to parent
+      displayCard.appendChild(blogTitle);
+      displayCard.appendChild(userName);
+      displayCard.appendChild(createdDate);
+      displayCard.appendChild(blogContent);
 
-    mainPanel.appendChild(displayCard);
+      // Apply parent to main panel
+      mainPanel.appendChild(displayCard);
+    }
   }
 }
 
 // Create HTML element for parent div from passed object and property
 function createBlogPostElement(blogPostObject, blogPostProperty) {
+  // Use parameter-to-class-name map for css assignment
   const blogPostElementToClassMap = {
     blogTitle: "blog-card-title",
     userName: "blog-card-username",
-    createdDate: 'blog-created-date',
-    blogContent: "blog-card-content"
+    createdDate: "blog-created-date",
+    blogContent: "blog-card-content",
   };
 
+  // Create element and apply css
   const element = document.createElement("div");
   element.classList.add(blogPostElementToClassMap[blogPostProperty]);
 
-  switch(blogPostProperty) {
-    case 'userName':
+  // Populate inner HTML based on passed property
+  switch (blogPostProperty) {
+    case "userName":
       element.innerHTML = `Written by ${blogPostObject[blogPostProperty]}`;
       break;
-    case 'createdDate':
+    case "createdDate":
       const date = new Date(blogPostObject[blogPostProperty]);
       element.innerHTML = date.toLocaleString();
       break;
     default:
       element.innerHTML = blogPostObject[blogPostProperty];
+      break;
   }
 
   return element;
